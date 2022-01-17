43688469bd933f76bb444e8cfd2dc35963e8d748
@namhv <namhv@vmodev.com>
2022-01-17 14:16:53 +0700

    create component select

--- imports/svgs.tsx
+++ imports/svgs.tsx
@@ -231,3 +231,14 @@ export const ChevronDown = () => (
     <path d="M7 10L12 15L17 10H7Z" fill="#818389" />
   </svg>
 )
+export const ChevronUp = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M17 14L12 9L7 14L17 14Z" fill="#818389" />
+  </svg>
+)
