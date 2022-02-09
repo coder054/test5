2478c74c2fba9a8614ab0114dbe3adc8c0d4145b
@namhv <namhv@vmodev.com>
2022-02-09 15:17:59 +0700

    fix title select

--- pages/test.tsx
+++ pages/test.tsx
@@ -19,10 +19,10 @@ const Test = () => {
           className=""
           defaultValue={'3'}
           options={[
-            { title: 'item 1', value: '1' },
-            { title: 'item 2', value: '2' },
-            { title: 'item 3', value: '3' },
-            { title: 'item 4', value: '4' },
+            { label: 'item 1', value: '1' },
+            { label: 'item 2', value: '2' },
+            { label: 'item 3', value: '3' },
+            { label: 'item 4', value: '4' },
           ]}
         />
       </div>
