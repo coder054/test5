756da0cd88295e803ad84e9ed5786214ba89886d
@namhv <namhv@vmodev.com>
2022-02-17 17:26:17 +0700

    responsive signup form

--- components/layout-signup-form/index.tsx
+++ components/layout-signup-form/index.tsx
@@ -7,20 +7,24 @@ interface LayoutLandingProps {
 
 export const LayoutSignupForm = ({ authen, children }: LayoutLandingProps) => {
   return (
-    <div className="h-screen w-full bg-[#000000] relative">
+    <div className="min-h-screen w-full bg-[#000000] relative overflow-hidden">
       {authen && (
         <div
-          className={`${cls.backgroundAuthen} w-screen h-screen absolute left-0 bottom-0`}
+          className={`${cls.backgroundAuthen} w-screen lg:h-screen absolute lg:left-0 lg:bottom-0`}
         ></div>
       )}
       <div
-        className={`${cls.imageBackground} absolute w-[808px] h-[808px]`}
+        className={`${cls.imageBackground} absolute w-0 lg:w-[808px] h-0 lg:h-[808px]`}
       ></div>
-      <div className="absolute right-0">{children}</div>
-      <div className="w-[58.5px] h-[58.5px] bg-[#FF9607] rounded-full absolute ml-[500px] mt-[70px]"></div>
-      <div className="w-[32px] h-[32px] bg-[#FFFFFF] rounded-full absolute ml-[153px] mt-[120px]"></div>
-      <div className="w-[93px] h-[93px] bg-[#09E099] rounded-full absolute -ml-[46.5px] mt-[400px]"></div>
-      <div className="w-[39px] h-[39px] bg-[#4654EA] rounded-full absolute ml-[591px] mt-[800px]"></div>
+      <div className="w-full md:right-0">{children}</div>
+      <div className="lg:hidden">
+        <div className={`${cls.vectorTop} pointer-events-none`}></div>
+        <div className={`${cls.vectorBottom} `}></div>
+      </div>
+      <div className=" lg:w-[58.5px] lg:h-[58.5px] bg-[#FF9607] rounded-full absolute ml-[500px] mt-[70px]"></div>
+      <div className=" lg:w-[32px] lg:h-[32px] bg-[#FFFFFF] rounded-full absolute ml-[153px] mt-[120px]"></div>
+      <div className=" lg:w-[93px] lg:h-[93px] bg-[#09E099] rounded-full absolute -ml-[46.5px] mt-[400px]"></div>
+      <div className=" lg:w-[39px] lg:h-[39px] bg-[#4654EA] rounded-full absolute ml-[591px] mt-[800px]"></div>
     </div>
   )
 }
