7df62686b2a4db108162b9b7f92231a9c11aed9e
vincent <vincent@Zporter.co>
2022-02-22 15:46:38 +0700

    remove screen tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -62,11 +62,6 @@ module.exports = {
 
     screens: {
       mobileM: '375px',
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px',
       ...defaultTheme.screens,
 
       max_sm: { max: '640px' },
