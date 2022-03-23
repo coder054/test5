4096aec81d6d9f12e9313069f615ad4d89941f45
Austin <anhdt@vmodev.com>
2022-03-23 14:21:08 +0700

    update

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,13 +1,11 @@
-import { requireNotAuth } from 'src/config/firebase-admin'
-import { LayoutLanding } from 'src/components/layout-landing/layout-landing'
-import { useAuth } from 'src/module/authen/auth/AuthContext'
-import { SignUp } from 'src/module/authen/signup/signup'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { LayoutLanding } from 'src/components/layout-landing/layout-landing'
+import { requireNotAuth } from 'src/config/firebase-admin'
+import { SignUp } from 'src/module/authen/signup/signup'
 
-const SignUpPage = () => {
+export default function () {
   const router = useRouter()
-  const { currentUser } = useAuth()  
   return (
     <LayoutLanding authen>
       <Head>
@@ -18,8 +16,6 @@ const SignUpPage = () => {
     </LayoutLanding>
   )
 }
-
-export default SignUpPage
 
 export const getServerSideProps: any = async ({ req, res }) => {
   await requireNotAuth(req as any, res as any)
