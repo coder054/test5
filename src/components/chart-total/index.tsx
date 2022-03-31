5d17b00c2082cbee8c18e630d97835f6867a0a18
@namhv <namhv@vmodev.com>
2022-03-31 09:41:19 +0700

    recustom chart training

--- src/components/chart-total/index.tsx
+++ src/components/chart-total/index.tsx
@@ -75,40 +75,55 @@ export const ChartTotal = ({ index, chart }: ChartTotalProps) => {
         <p className="font-bold text-[16px] text-center">Total</p>
       )}
 
-      <div className="flex w-[92px] mt-[32px] justify-between gap-1">
-        <div className="flex-1 relative">
-          <div className="h-[164px]">
-            <p className="text-[12px] text-center">{totalYou}h</p>
-            {keyYou &&
-              keyYou.map((key, index) => (
-                <div
-                  style={{
-                    height: `${youPercent[index] * 1.6}px`,
-                  }}
-                  className="bg-[#4654EA] rounded-[4px] text-[16px] mb-[4px] flex items-center"
-                >
-                  <p className="mx-auto">{key}</p>
+      <div className="w-[92px] h-[208px] relative">
+        <div className="flex w-full mt-[32px] gap-1 absolute top-0 h-full">
+          <div className="flex-1 relative">
+            <div className="h-[184px] absolute bottom-[28px] w-full">
+              <div className="absolute bottom-0 w-full">
+                <p className="text-[12px] text-center -mb-[4px]">{totalYou}h</p>
+                <div className="w-full">
+                  {keyYou &&
+                    keyYou.map((key, index) => (
+                      <div
+                        style={{
+                          height: `${youPercent[index] * 1.6}px`,
+                        }}
+                        className={`bg-[#4654EA] rounded-[4px] text-[16px] mt-[4px] flex items-center ${cls.minHeight}`}
+                      >
+                        <p className="mx-auto">{key}</p>
+                      </div>
+                    ))}
                 </div>
-              ))}
+              </div>
+            </div>
+            <p className="text-[#4654EA] text-[14px] mt-[8px] ml-[6px] text-center absolute bottom-0">
+              YOU
+            </p>
           </div>
-          <p className="text-[#4654EA] text-[14px] mt-[8px] text-center absolute bottom-0">
-            YOU
-          </p>
-        </div>
-        <div className="flex-1">
-          <p className="text-[12px] text-center">{totalAvg}h</p>
-          <div className="h-[164px] flex-col">
-            {keyAvg &&
-              keyAvg.map((key, index) => (
-                <div
-                  style={{ height: `${avgPercent[index] * 1.6}px` }}
-                  className={`${cls.avg} rounded-[4px] text-[16px] mb-[4px] flex items-center`}
-                >
-                  <p className="mx-auto">{key}</p>
+
+          <div className="flex-1 relative">
+            <div className="h-[184px] absolute bottom-[28px] w-full">
+              <div className="absolute bottom-0 w-full">
+                <p className="text-[12px] text-center -mb-[4px]">{totalAvg}h</p>
+                <div className="w-full">
+                  {keyAvg &&
+                    keyAvg.map((key, index) => (
+                      <div
+                        style={{
+                          height: `${avgPercent[index] * 1.6}px`,
+                        }}
+                        className={` ${cls.avg} rounded-[4px] text-[16px] mt-[4px] flex items-center`}
+                      >
+                        <p className="mx-auto">{key}</p>
+                      </div>
+                    ))}
                 </div>
-              ))}
+              </div>
+            </div>
+            <p className="text-[#A2A5AD] text-[14px] mt-[8px] ml-[6px] text-center absolute bottom-0">
+              AVG
+            </p>
           </div>
-          <p className="text-[#A2A5AD] text-[14px] mt-[8px] text-center">AVG</p>
         </div>
       </div>
     </div>
