2422a2910e1840ea35a9b7cd0d8fe18c92ba46ec
Austin <anhdt@vmodev.com>
2022-04-06 15:15:19 +0700

    config firebase cloud messaging

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,10 @@
+importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-messaging.js')
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyD7JfGqgH_Y4opp0B7zgXEJe9FFa26phtE',
+  projectId: 'zporter-dev',
+  messagingSenderId: '357104839495',
+  appId: '1:357104839495:web:924336770c1ea9b606fa62',
+})
+const messaging = firebase.messaging()
