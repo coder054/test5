11c7252e9a1e5b9b5ddf849e328ff2f56513ad9d
Austin <anhdt@vmodev.com>
2022-05-09 17:14:19 +0700

    update signup page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { GuestGuard } from 'src/components/authentication/guest-guard'
 import { LayoutLanding } from 'src/components/layout-landing/layout-landing'
 import { requireNotAuth } from 'src/config/firebase-admin'
 import { SignUp } from 'src/modules/authentication/signup/signup'
@@ -7,17 +8,19 @@ import { SignUp } from 'src/modules/authentication/signup/signup'
 export default function () {
   const router = useRouter()
   return (
-    <LayoutLanding authen>
-      <Head>
-        <title>Zporter</title>
-        <link rel="icon" type="image/png" href="/favicon.png" />
-      </Head>
-      <SignUp />
-    </LayoutLanding>
+    <GuestGuard>
+      <LayoutLanding authen>
+        <Head>
+          <title>Zporter</title>
+          <link rel="icon" type="image/png" href="/favicon.png" />
+        </Head>
+        <SignUp />
+      </LayoutLanding>
+    </GuestGuard>
   )
 }
 
-export const getServerSideProps: any = async ({ req, res }) => {
-  await requireNotAuth(req as any, res as any)
-  return { props: {} }
-}
+// export const getServerSideProps: any = async ({ req, res }) => {
+//   await requireNotAuth(req as any, res as any)
+//   return { props: {} }
+// }
