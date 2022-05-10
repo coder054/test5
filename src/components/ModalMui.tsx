b53ae5c0f765559257eb9f4526e10f7d8f0c2820
Austin <anhdt@vmodev.com>
2022-05-10 17:21:15 +0700

    update ModalMui component

--- src/components/ModalMui.tsx
+++ src/components/ModalMui.tsx
@@ -1,6 +1,7 @@
-import Modal from '@mui/material/Modal'
 import Box from '@mui/material/Box'
+import Modal from '@mui/material/Modal'
 import { CSSObject } from '@mui/system'
+import { XIcon } from 'src/components/icons'
 
 const style = {
   borderRadius: 1,
@@ -10,20 +11,44 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 500,
   bgcolor: '#1E1F24',
-  p: 4,
+  p: {
+    xs: 2,
+    xl: 4,
+  },
+  maxWidth: 'calc(100vw - 32px)',
 }
 
 type MyModalProps = {
   sx?: CSSObject
+  showXIcon?: boolean
   isOpen: boolean
   onClose: (value: boolean) => void
   children: React.ReactElement
 }
 
-export const ModalMui = ({ isOpen, onClose, children, sx }: MyModalProps) => {
+export const ModalMui = ({
+  isOpen,
+  onClose,
+  children,
+  sx,
+  showXIcon,
+}: MyModalProps) => {
   return (
     <Modal open={isOpen} onClose={() => onClose(false)}>
-      <Box sx={{ ...style, ...sx }}>{children}</Box>
+      <Box sx={{ ...style, ...sx }}>
+        {showXIcon && (
+          <button
+            type="button"
+            onClick={() => {
+              onClose(false)
+            }}
+            className="absolute right-4 top-4 z-10 "
+          >
+            <XIcon />
+          </button>
+        )}
+        {children}
+      </Box>
     </Modal>
   )
 }
