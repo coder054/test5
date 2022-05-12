225179757ed1da6287281eed6c858d17d56784ce
Austin <anhdt@vmodev.com>
2022-05-12 13:32:35 +0700

    fix

--- src/components/dashboard/dashboard-navbar.tsx
+++ src/components/dashboard/dashboard-navbar.tsx
@@ -518,8 +518,8 @@ export const DashboardNavbar: FC<DashboardNavbarProps> = (props) => {
           </IconButton>
           <Box sx={{ flexGrow: 1 }} />
 
-          {!authenticated ? (
-            <div className=" animate-appear  ">
+          {true ? (
+            <div className=" animate-appear flex ">
               <Link href="/sign-in">
                 <a>
                   <button
