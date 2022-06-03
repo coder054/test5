5bf9e292c9aa6ce50b05c13b1140c7c57ed1e087
Austin <anhdt@vmodev.com>
2022-06-03 16:55:37 +0700

    a

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { LayoutLanding } from 'src/components/layout-landing/layout-landing'
+import { requireNotAuth } from 'src/config/firebase-admin'
 import { SignUp } from 'src/modules/authentication/signup/signup'
 
 export default function () {
@@ -12,4 +13,9 @@ export default function () {
       <SignUp />
     </LayoutLanding>
   )
+}
+
+export const getServerSideProps: any = async ({ req, res }) => {
+  await requireNotAuth(req as any, res as any)
+  return { props: {} }
 }
